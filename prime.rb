0e97ceed25f84179fc06a77483dd4cdfefc53fa9
Jerry Charles <jerrycharles586@gmail.com>
2019-04-18 00:39:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,13 @@ def prime?(n)
   num = (1...n).to_a
   num.shift
   number = []
-  num.each |digit|
+  num.each {|digit|
   if n % digit !=0
     number << true
   else
     number << false
   end
+  }
   if (!number.include?(false) && n > 1)
     return true
   else
